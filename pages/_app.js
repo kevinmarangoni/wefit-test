0dad7be94de9219e71cd4fb8a47a7b40e9013909
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-01-31 19:21:01 -0300

    adds theme context

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
+import Theme from "/src/context/Theme.jsx";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Theme>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
 
-export default MyApp
+export default MyApp;
