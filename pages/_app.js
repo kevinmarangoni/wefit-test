8be79676a94e961321d9405b7511c5c5a734dcfd
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-02-01 00:23:50 -0300

    adds context to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import Theme from "/src/context/Theme.jsx";
 import Head from "next/head";
 
+import CartProvider from "/src/context/Cart.jsx"
+
 function MyApp({ Component, pageProps }) {
   return (
     <Theme>
       <Head>
         <title>WeMovies</title>
       </Head>
+      <CartProvider>
       <Component {...pageProps} />
+      </CartProvider>
     </Theme>
   );
 }
