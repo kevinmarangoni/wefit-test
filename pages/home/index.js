70f9d8426993ec72556c337c5f8b2f6473a9af8c
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-01-31 22:24:31 -0300

    creates page

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '/src/components/common/header/header'
+import Body from '/src/components/pages/home/body'
+import Layout from '/src/components/common/layout/Layout'
 
 const Home = () => {
+
   return (
-    <div>Home</div>
+    <Layout>
+      <Header />
+      <Body />
+    </Layout>
   )
 }
 
